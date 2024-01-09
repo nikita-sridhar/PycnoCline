909c04b74d0f976ee4018d8daab3b7078a794bdb
Niki Sridhar <nikitasridhar12@gmail.com>
2024-01-09 13:15:33 +0530

    pyc position

--- scripts/cleaning_full.R
+++ scripts/cleaning_full.R
@@ -1,16 +1,26 @@
 library(tidyverse)
 
+###########
 #load files
+###########
 kelp <- read_csv("data/raw data/RawData - Kelp.csv")
 behavior <- read_csv("data/raw data/RawData - Behavior.csv")
 
-
-#find avg pyc position in a trial - will be used later, esp imp for active treatment
+#################################
+#find avg pyc position in a trial
+#################################
+#will be used later, esp imp for active treatment
 
 #on per time pt scale
 pyc_position <- behavior %>%
-  select(Date, Day_numrecord,Time,Trial,Treatment,Cline,Pyc_position) %>%
-  mutate(Pyc_position = ifelse(Pyc_position == 1, Cline, NA)) 
+  select(Date, Day_numrecord,Time,Trial,Treatment, Treatment_ID, Cline, Pyc_position) %>%
+  group_by(Trial, Treatment, Day_numrecord) %>%
+  mutate(#If active treatment, if 1 for pyc position, change it to cline num. 
+         #if 0, change to cline num where it IS 1. if not active, leave the same
+         Pyc_position = ifelse(Treatment == "Active", ifelse(Pyc_position == 1, Cline, Pyc_), Pyc_position), 
+         #for caged/control treatments, change pyc position to 5.5 for all. if 
+         #not caged/control, leave same
+         Pyc_position = ifelse(Treatment %in% c("Caged", "Control"),5.5,Pyc_position)) 
 #on per trial scale
 avg_pyc_position <- pyc_position %>%
   group_by(Trial, Treatment) %>%
@@ -18,13 +28,27 @@ avg_pyc_position <- pyc_position %>%
   ungroup() %>%
   mutate(avg_pyc_position = ifelse(Treatment %in% c("Caged", "Control"),5.5, avg_pyc_position))
 
+#scratch to be added to line 20
+#pyc position for all active is equal to the value of that cline where the corresponding pycno position is 1
+#index cline by pyc position equal to 1
+Pyc_position = cline[pyc_position == 1]
 
-#for distance from pyc calculation, can't use average pyc value as that is on a whole trial
-#scale - instead at each time point need to calculate distance, then average this
-#to get back on the trial scale. cline num and kelp id synonymous
+
+###############################
+#distance from pyc calculation  
+###############################
+
+#can't use average pyc value as that is on a whole trial scale - instead at each 
+#time point need to calculate distance, then average this to get back on the trial 
+#scale. cline num and kelp id synonymous
+
 dist_from_pyc <- behavior %>%
   group_by(Trial, Treatment, Day_numrecord) %>%
   summarise(dist_from_pyc = abs())
+
+##############################
+#adding new columns to kelp df
+##############################
 
 kelp <- kelp %>%
   #adding column for avg pyc position
@@ -34,7 +58,7 @@ kelp <- kelp %>%
          weight_pcnt_change = ((Kelp_weight_before_g - Kelp_weight_after_g)/
                                 Kelp_weight_before_g)*100,
          #column for blade's upstream/downstream position relative to pycno
-         up_down = ifelse(Kelp_ID < avg_pyc_position,"Upstream","Downstream"))
+         up_down = ifelse(Kelp_ID < avg_pyc_position,"Up","Down"))
    
 
 
