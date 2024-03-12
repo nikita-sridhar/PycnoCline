2e2a12d4fb8e1b8d2ba644bafceb66888ad154d4
Niki Sridhar <nikitasridhar12@gmail.com>
2024-03-12 16:37:07 +0530

    format

--- __main.R
+++ __main.R
@@ -10,15 +10,15 @@ source(here::here("./scripts/01_manipulation.R"))
 #Step 2 - Analyze data
 source(here::here("./scripts/02_analysis.R"))
 #Step 3 - Plot data
-source(here::here("./scripts/03_make_plots.R"))
+#source(here::here("./scripts/03_make_plots.R"))
 
 
 #Save files
-write.csv(kelp_clean, file = "data/processed data/cleaned data/kelp_clean.csv")
-write.csv(behavior_clean, file = "data/processed data/cleaned data/behavior_clean.csv")
+#write.csv(kelp_clean, file = "data/processed data/cleaned data/kelp_clean.csv")
+#write.csv(behavior_clean, file = "data/processed data/cleaned data/behavior_clean.csv")
 
-write.csv(kelp, file = "data/processed data/manipulated data/kelp.csv")
-write.csv(urch_behavior, file = "data/processed data/manipulated data/urch_behavior.csv")
+#write.csv(kelp, file = "data/processed data/manipulated data/kelp.csv")
+#write.csv(urch_behavior, file = "data/processed data/manipulated data/urch_behavior.csv")
 
 #clear environment
-rm(list = ls()); gc()
+#rm(list = ls()); gc()
